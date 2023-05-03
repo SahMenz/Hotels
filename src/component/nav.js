0d99e08f3cff13ea3bba86a5b681337166d279dc
Sah Menz <mensahemma670@gmail.com>
2023-05-03 14:23:43 +0100

    first comiz

--- src/component/nav.js
+++ src/component/nav.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import LogoBlock from '../images/LogoBlock.svg'
-import city from '../images/city.svg'
 import '../component/nav.css'
 
 function Nav() {
@@ -9,7 +8,7 @@ function Nav() {
         <img src={LogoBlock} alt="" />
         <div className="side">
             <div className="searchcontainer">
-                <input type="search"  placeholder='Search for hotel, city, categories  <img src={city} alt="" />'/>
+                <input type="search"  placeholder='Search for hotel, city, categories       &#xf64f; city     &#xf133; Date        &#xf13e; Admit'  className='large'/>
             </div>
             <div className="navigateto"></div>
         </div>
