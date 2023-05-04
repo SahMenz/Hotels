1be1243f0485931c8ac29ba72796a999d8a9aaeb
Sah Menz <mensahemma670@gmail.com>
2023-05-04 12:09:14 +0100

    second commit

--- src/component/nav.js
+++ src/component/nav.js
@@ -1,17 +1,43 @@
 import React from 'react';
 import LogoBlock from '../images/LogoBlock.svg'
 import '../component/nav.css'
+import tie from '../images/tie.svg'
+import plus from '../images/plus.svg'
+import city from '../images/city.svg'
+import date from '../images/date.svg'
+import admit from '../images/admit.svg'
+import stroke from '../images/stroke.svg'
+import search from '../images/search.svg'
 
 function Nav() {
   return (
     <div className='general'>
-        <img src={LogoBlock} alt="" />
-        <div className="side">
-            <div className="searchcontainer">
-                <input type="search"  placeholder='Search for hotel, city, categories       &#xf64f; city     &#xf133; Date        &#xf13e; Admit'  className='large'/>
+      <img src={LogoBlock} alt="" />
+      <div className="side">
+          <div className="searchcontainer">
+            <div className="searchee">
+              <img src={search} alt="" />
+              <input type="search"  placeholder='Search for hotel, city, categories'  className='large'/>
             </div>
-            <div className="navigateto"></div>
-        </div>
+            <div className="suprise">
+              <div className="supone"> <img src={city} alt="" /> City</div>
+              <img src={stroke} alt="" />
+              <div className="supone"> <img src={date} alt="" /> Date</div>
+              <img src={stroke} alt="" />
+              <div className="supone"> <img src={admit} alt="" /> Admit</div>
+            </div>
+          </div>
+          <div className="navigateto">
+            <ul className='notordered'>
+              <li className="lister">Explore</li>
+              <li className="lister">Learn</li>
+              <li className="lister">Support</li>
+              <li className="lister listerone">LIST HOTEL</li>
+              <li className="lister"><img src={tie} alt="#" /></li>
+              <li className="lister"><img src={plus} alt="#" /></li>
+            </ul>
+          </div>
+      </div>
     </div>
   )
 }
