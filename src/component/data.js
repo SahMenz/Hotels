d7604ab327a3026a5919211e201572d5a025746b
Sah Menz <mensahemma670@gmail.com>
2023-05-05 11:10:00 +0100

    committrer

--- src/component/data.js
+++ src/component/data.js
@@ -2,11 +2,32 @@ import greek from '../images/greek.svg'
 import silk from '../images/silk.svg'
 import gahvah from '../images/gahvah.svg'
 import moerna from '../images/moerna.svg'
+import jordon from '../images/jordon.svg'
 
 export const sliding = [
     {
         img: greek,
         hotelname: 'Greek Taverna',
         hotelratings: '0ver 10+ listing'
+    },
+    {
+        img: gahvah,
+        hotelname: 'Gahvah Hotel',
+        hotelratings: '0ver 10+ listing'
+    },
+    {
+        img: silk,
+        hotelname: 'Silk Valley',
+        hotelratings: '0ver 10+ listing'
+    },
+    {
+        img: jordon,
+        hotelname: 'Jayden’s House',
+        hotelratings: '0ver 40+ listing'
+    },
+    {
+        img: moerna,
+        hotelname: 'Moerana Hotel',
+        hotelratings: '0ver 5+ listing'
     },
 ]
--- src/component/hero.js
+++ src/component/hero.js
@@ -1,6 +1,9 @@
 import React from 'react'
 import styles from './hero.module.css'
 import mosque from '../images/mosque.svg'
+import { sliding } from './data.js'
+import Labelnew from '../images/Labelnew.svg'
+import linestroke from '../images/linestroke.svg'
 
 function Hero() {
   return (
@@ -12,6 +15,20 @@ function Hero() {
             </div>
             <img src={mosque} alt="" className={styles.heroimage}/>
             <div className={styles.slider}>
+              {sliding.map((slider) => (
+                <>
+                  <div className={styles.slidercontainer}>
+                    <img src={slider.img} alt="" />
+                    <img src={linestroke} alt="" />
+                    <div className={styles.namesandratings}>
+                      <div className={styles.names}>{slider.hotelname}</div>
+                      <div className={styles.ratings}>{slider.hotelratings}</div>
+                    </div>
+                    <img src={Labelnew} alt="newlabel"  className={styles.placement}/>
+                  </div>
+                </>
+              ))}
+              
             </div>
         </div>
     </div>
