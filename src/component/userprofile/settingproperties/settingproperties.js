fd76f8b4df86eff94054b2355701dbe05e926e97
Sah Menz <mensahemma670@gmail.com>
2023-07-24 23:08:16 +0100

    im using formik here

--- src/component/userprofile/settingproperties/settingproperties.js
+++ src/component/userprofile/settingproperties/settingproperties.js
@@ -2,15 +2,23 @@ import React, { useState } from 'react'
 import Styles from '../settingproperties/settingproperties.module.css'
 import IconsReactjs  from 'icons-reactjs';
 // import nikelogo from '../../../images/nikelogo.png'
-import { Formik, Form, Field, ErrorMessage } from 'formik';
-
+import { useFormik } from 'formik';
 
 function Settingproperties() {
 
-    const [number, setNumber] = useState("+2349060758772");
-    const [emailchange, setEmailchange] = useState("mensahemma670@gmail.com");
-    const [firstname, setFirstname] = useState("Emmanuel");
-    const [lastname, setLastname] = useState("Mensah");
+    const formik = useFormik ({
+        initialValues: {
+            firstname: 'wer',
+            surname: '',
+            formEmail: '',
+            formPhone: '',
+        },
+        onSubmit: values => {
+            console.log('Form data', values)
+        }
+    })
+
+    // console.log('From values:', formik.values)
 
   return (
     <div className={Styles.settingprofileform}>
@@ -19,25 +27,24 @@ function Settingproperties() {
                 {/* <img src={nikelogo} alt="" /> */}
             </div>
             <div className={Styles.avatarcredentialsnames}>
-                <h1 className={Styles.usersname}>{firstname} {lastname}</h1>
+                {/* <h1 className={Styles.usersname}>{firstname} {lastname}</h1> */}
                 <h3 className={Styles.gmail}>
                     <IconsReactjs 
                     icon={'email'}
                     fontSize={'2rem'} 
                     color={'#6B705C'}
-                    /> 
-                    {emailchange}</h3>
+                    /></h3>
                 <h3 className={Styles.phonenumber}>
                     <IconsReactjs 
                     icon={'call'} 
                     fontSize={'2rem'} 
                     color={'#6B705C'}
-                    />{number}</h3>
+                    /></h3>
             </div>
             <div className={Styles.avatarupload}>Upload new avatar</div>
         </div>
         <div className={Styles.settingformcontainer}>
-            <Formik
+            {/* <Formik
                 initialValues={{ email: '', text: '', number: '' }}
                 validate={values => {
                     const errors = {};
@@ -70,13 +77,11 @@ function Settingproperties() {
                         <div className={Styles.dividednames}>
                             <div className={Styles.dividednameone}>
                                 <label for="texts">FIRST NAME</label>
-                                {/* <input type="text" id="form-name" name='form-name' required/> */}
                                 <Field type="text" name="texts" value={firstname} onChange={(e) => setFirstname(e.target.value)} required/>
                                 <ErrorMessage name="texts" />
                             </div>
                             <div className={Styles.dividednametwo}>
                                 <label for="text">LAST NAME</label>
-                                {/* <input type="text" id="form-surname" name='form-surname'required/> */}
                                 <Field type="text" name="text" value={lastname} onChange={(e) => setLastname(e.target.value)} required/>
                                 <ErrorMessage name="text" /> 
                             </div>
@@ -84,16 +89,68 @@ function Settingproperties() {
                         
                         <label for="email">EMAIL ADDRESS</label>
                         <Field type="email" name="email" value={emailchange} onChange={(e) => setEmailchange(e.target.value)} required/>
-                        {/* <input type="email" id="form-email" name='form-email' required/> */}
-                        {/* <ErrorMessage name="email"/> */}
         
                         <label for="phonenumber">PHONE NUMBER</label>
-                        {/* <input type="number" name="phonenumber" id="phone" required/> */}
                         <Field type="number" name="phonenumber" value={number} onChange={(e) => setNumber(e.target.value)} required/>
                         <ErrorMessage name="phonenumber" />
                     </form>
                 )}
-            </Formik>
+            </Formik> */}
+            <form onSubmit={formik.handleSubmit}>
+                <div className={Styles.settingheaders}>
+                    BASIC INFO
+                    <div className={Styles.bottomm}>
+                        <input type="reset"  value="CANCEL" className={Styles.cancelbutton} />
+                        <input type="submit" value="SAVE" className={Styles.savebutton} />
+                    </div>
+                </div>
+                
+                
+                <div className={Styles.dividednames}>
+                    <div className={Styles.dividednameone}>
+                        <label htmlFor="firstname">FIRST NAME</label>
+                        <input 
+                            type="text" 
+                            id="firstname" 
+                            name='firstname' 
+                            placeholder="firstname" 
+                            onChange={formik.handleChange} 
+                            value={formik.values.firstname}
+                        />
+                    </div>
+                    <div className={Styles.dividednametwo}>
+                        <label htmlFor="surname">LAST NAME</label>
+                        <input 
+                            type="text" 
+                            id="surname" 
+                            name='surname' 
+                            placeholder="surname" 
+                            onChange={formik.handleChange} 
+                            value={formik.values.surname} 
+                        />
+                    </div>
+                </div>
+                
+                <label htmlFor="formEmail" class="design">Email Address</label>
+                <input 
+                    type="email" 
+                    id="formEmail" 
+                    name='formEmail' 
+                    placeholder="e.g. stephenking@lorem.com" 
+                    onChange={formik.handleChange} 
+                    value={formik.values.formEmail}
+                />
+
+                <label htmlFor="formPhone">PHONE NUMBER</label>
+                <input 
+                    type="number" 
+                    id="formPhone" 
+                    name='formPhone' 
+                    placeholder="e.g. +1 234 567 890" 
+                    onChange={formik.handleChange} 
+                    value={formik.values.formPhone}
+                />
+            </form>
         </div>
     </div>
   )
